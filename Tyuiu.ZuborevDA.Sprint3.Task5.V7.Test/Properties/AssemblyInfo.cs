3b0ef519d78888a06279da0f4274c3caea13bbf2
Dmitriy Zuborev <Tyuiu.ZuborevDA@outlook.com>
2023-10-23 13:44:28 +0500

    Task5

---
+++ Tyuiu.ZuborevDA.Sprint3.Task5.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZuborevDA.Sprint3.Task5.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZuborevDA.Sprint3.Task5.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("491803b9-ac6c-4ca9-96eb-610c03568a67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZuborevDA.Sprint3.Task5.V7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZuborevDA.Sprint3.Task5.V7.Lib;
+
+namespace Tyuiu.ZuborevDA.Sprint3.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Зуборев Д. А. | АСОиУБ-23-2";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                  *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                      *");
+            Console.WriteLine("* Задание #5                                                                 *");
+            Console.WriteLine("* Вариант #7                                                                 *");
+            Console.WriteLine("* Выполнил: Зуборев Дмитрий Александрович | АСОиУБ-23-2                      *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+
+            Console.WriteLine($"Переменная X = {x}");
+            Console.WriteLine($"Старт шага первой суммы ряда {startValue1}");
+            Console.WriteLine($"Старт шага второй суммы ряда {startValue2}");
+            Console.WriteLine($"Конец шага первой суммы ряда {stopValue1}");
+            Console.WriteLine($"Конец шага второй суммы ряда {stopValue2}");
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine($"Сумма ряда = {ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2)}");
+            Console.ReadKey();
+        }
+    }
+}
